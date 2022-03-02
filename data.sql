1f0458517156ac9c504a5737f4cacb0bc0bb5739
Afiz Savage <savageafiz9@gmail.com>
2022-03-02 18:06:46 +0000

    Implemented optionall changes requested by reviewer

--- data.sql
+++ data.sql
@@ -6,7 +6,7 @@ INSERT INTO animals VALUES (3, 'Pikachu', cast('2021/01/7' as date), 1, '0', 15.
 INSERT INTO animals VALUES (4, 'Devimon', cast('2017/06/12' as date), 5, '1', 11);
 INSERT INTO animals VALUES (5, 'Charmander', '2020/02/08', 0, '1', -11);
 INSERT INTO animals VALUES (6, 'Plantmon', '2022/11/15', 2, '1', -5.7);
-INSERT INTO animals VALUES (7, 'Squirtie', '1993/04/02', 3, '0', 12.13);
+INSERT INTO animals VALUES (7, 'Squirtie', '1993/04/02', 3, '0', -12.13);
 INSERT INTO animals VALUES (8, 'Angemon', '2005/06/12', 1, '1', -45);
 INSERT INTO animals VALUES (9, 'Boarmon', '2005/06/7', 7, '1', 20.4);
 INSERT INTO animals VALUES (10, 'Blossom', '1998/10/13', 3, '1', 17);
