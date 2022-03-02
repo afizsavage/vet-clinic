789cbfe1112adf829a82b277e705e1ff8ef07a37
Afiz Savage <savageafiz9@gmail.com>
2022-03-02 12:48:22 +0000

    Wrote qurries for animal table using aggregate functions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,9 @@ SELECT name, escape_attempts FROM animals WHERE weight > 10.5;
 SELECT * FROM animals WHERE neutered = '1';
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT * FROM animals WHERE weight BETWEEN 10.3 AND 17.4;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts < 1;
+SELECT AVG(weight) FROM animals;
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered
+SELECT neutered, MIN(weight), MAX(weight) FROM animals GROUP BY neutered;
+SELECT neutered, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990/12/31' AND '2000/01/01' GROUP BY neutered;
