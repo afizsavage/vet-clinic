2c2b833a9b0c9913c3cd62ca03f13d476ec46d30
Afiz Savage <savageafiz9@gmail.com>
2022-03-02 17:10:20 +0000

    implemented changes required by reviewer

--- queries.sql
+++ queries.sql
@@ -12,5 +12,31 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts < 1;
 SELECT AVG(weight) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered
-SELECT neutered, MIN(weight), MAX(weight) FROM animals GROUP BY neutered;
-SELECT neutered, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990/12/31' AND '2000/01/01' GROUP BY neutered;
+SELECT species, MIN(weight), MAX(weight) FROM animals GROUP BY species;
+SELECT neutered, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990/01/01' AND '2000/31/12' GROUP BY neutered;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT species FROM animals;
+ROLLBACK;
+SELECT species FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon'; 
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL; 
+COMMIT;
+SELECT species from animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT species from animals;
+
+BEGIN;
+DELETE FROM animals WHERE  date_of_birth > '2022-01-01';
+SAVEPOINT delete_by_date;
+UPDATE animals SET weight = weight * -1;
+SELECT weight FROM animals;
+ROLLBACK TO delete_by_date;
+UPDATE animals SET weight = weight * -1 WHERE weight < 0; 
+COMMIT;
