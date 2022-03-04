4bec9bd78ef5b36eff5aaa0656b01bf5b11bf712
Afiz Savage <savageafiz9@gmail.com>
2022-03-04 16:09:16 +0000

    Add join tables for many to many relationships

--- schema.sql
+++ schema.sql
@@ -26,6 +26,16 @@ CREATE TABLE vets (
     date_of_graduatation DATE
 );
 
+CREATE TABLE specializations (
+	species_id INT REFERENCES species (id),
+	vet_id INT REFERENCES vets (id)
+);
+
+CREATE TABLE visits (
+	animal_id INT REFERENCES animals (id),
+	vet_id INT REFERENCES vets (id),
+	date_of_visit DATE
+);
 
 ALTER TABLE animals ADD species VARCHAR(100);
 ALTER TABLE animals ALTER COLUMN id SET NOT NULL;
